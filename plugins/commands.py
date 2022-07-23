1d8025a72fa7d72f8c8ad00c6a257442aa68561b
AKOfficialTechy <109847405+AKOfficialTechy@users.noreply.github.com>
2022-07-23 13:59:52 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,7 +42,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('🔰Movie Updates🔰', url=f'https://t.me/+U8tOLSRjrNRjNjc1')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
             InlineKeyboardButton('♻️GROUP♻️', url='https://t.me/MalluMoviesGroup_MM')
@@ -88,7 +88,7 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('🔰Movie Updates🔰', url=f'https://t.me/+U8tOLSRjrNRjNjc1')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
             InlineKeyboardButton('♻️GROUP♻️', url='https://t.me/MalluMoviesGroup_MM')
